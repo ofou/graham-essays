6f52244f4b62c0011fa4df0a42b88134ebf35383
Omar Olivares <ofjolivares@gmail.com>
2021-04-26 22:15:40 -0400

    update dependencies and clean text

--- graham.py
+++ graham.py
@@ -4,15 +4,10 @@ import time
 import os.path
 import html2text
 import unidecode
+import regex as re
 
 """
-Download full collection of Paul Graham essays in EPUB & Markdown.
-
-Merge all the files using:
-    cat essays/*.md > graham.md
-
-Convert to EPUB them using pandoc
-    pandoc graham.md -o graham.epub -f gfm --metadata title="Paul Graham Essays"
+Download a collection of Paul Graham essays in EPUB & Markdown.
 """
 
 rss = feedparser.parse("http://www.aaronsw.com/2002/feeds/pgessays.rss")
@@ -21,24 +16,31 @@ h.ignore_images = True
 h.ignore_tables = True
 h.escape_all = True
 h.reference_links = True
-h.mark_code = False
+h.mark_code = True
 
 art_no = 1
 
 for entry in reversed(rss.entries):
     url = entry['link']
-    opener = urllib.request.FancyURLopener({})
 
     try:
-        with opener.open(url) as website:
+        with urllib.request.urlopen(url) as website:
             content = website.read().decode('unicode_escape', "utf-8")
             parsed = h.handle(content)
-            title = entry['title'].strip("/")
+            title = "_".join(entry['title'].split(
+                " ")).lower()
+            title = re.sub(r'[\W\s]+', '', title)
             with open(f"./essays/{art_no:03}_{title}.md", 'wb+') as file:
-                file.write(parsed.encode())
+                file.write(f"# {art_no:03} {entry['title']}\n\n".encode())
+                parsed = parsed.replace("[](index.html)  \n  \n", "")
+                parsed = [(p.replace("\n", "")
+                          if re.match(r"^[\p{Z}\s]*(?:[^\p{Z}\s][\p{Z}\s]*){5,100}$", p)
+                          else "\n"+p+"\n") for p in parsed.split("\n")]
+
+                file.write("".join(parsed).encode())
                 print(f"✅ {art_no:03} {entry['title']}")
 
     except Exception as e:
         print(f"❌ {art_no:03} {entry['title']}, ({e})")
     art_no += 1
-    time.sleep(1)  # 1s per article is ~4min, be nice with servers!
+    time.sleep(0.5)  # half sec/article is ~2min, be nice with servers!
